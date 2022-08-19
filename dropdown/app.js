afa4eaf41c45dac5bf8fc32dea20e23f842c3a52
jiwon9412 <ziwon9412@naver.com>
2022-08-19 10:44:40 +0900

    INIT: complete

--- dropdown/app.js
+++ dropdown/app.js
@@ -1,6 +1,9 @@
 const dropdownDiv = document.querySelector('.dropdown');
-
 const dropdownMenu = document.querySelector('.dropdown-menu');
+
+const dropdownOptions = document.querySelectorAll('.dropdown-option');
+const dropdownToggle = document.querySelector('.dropdown-toggle');
+const nextBtn = document.querySelector('.next-button');
 
 
 
@@ -8,3 +11,11 @@ dropdownDiv.addEventListener('click', function(){
     dropdownMenu.classList.toggle('show');
 })
 
+dropdownOptions.forEach((item) => {
+    item.addEventListener('click', function(){
+        //console.log(item.textContent);
+        dropdownToggle.textContent = item.textContent;
+        dropdownToggle.classList.add('selected');
+        nextBtn.disabled = false;
+    })
+})
